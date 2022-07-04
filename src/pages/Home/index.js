0a4fb495c31e8904d0a09475246f2032ac1e0417
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-07-03 21:32:05 -0300

    feat: JSX Structure

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Text>React Native</Text>
+      <Text>Ignite</Text>
+    </View>
+  )
+}
